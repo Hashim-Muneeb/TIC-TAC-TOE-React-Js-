2d8674324b97b85d4616a4532263e23e8a26bb30
Hashim-Muneeb <hashimmuneeb75@gmail.com>
2024-08-07 13:02:52 +0500

    GAME COMPLETED

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,68 @@ import Log from "./components/Log";
 import Player from "./components/PlayerInfo";
 import GameBoard from "./components/GameBoard";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  const [winner, setWinner] = useState(null);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setGameTurns((prevTurns) => {
-      const newTurn = {
-        square: { row: rowIndex, col: colIndex },
-        player: activePlayer,
-      };
-      return [...prevTurns, newTurn];
-    });
+    if (winner || gameBoard[rowIndex][colIndex]) return;
+
+    const newTurn = {
+      square: { row: rowIndex, col: colIndex },
+      player: activePlayer,
+    };
+
+    const updatedBoard = gameBoard.map((row, rIdx) =>
+      row.map((col, cIdx) =>
+        rIdx === rowIndex && cIdx === colIndex ? activePlayer : col
+      )
+    );
+
+    setGameTurns((prevTurns) => [...prevTurns, newTurn]);
+    setGameBoard(updatedBoard);
     setActivePlayer((prevPlayer) => (prevPlayer === 'X' ? 'O' : 'X'));
+
+    const gameWinner = checkWinner(updatedBoard);
+    if (gameWinner) {
+      setWinner(gameWinner);
+    }
+  }
+
+  function checkWinner(board) {
+    const winningCombinations = [
+      // Rows
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      // Columns
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      // Diagonals
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]],
+    ];
+
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (
+        board[a[0]][a[1]] &&
+        board[a[0]][a[1]] === board[b[0]][b[1]] &&
+        board[a[0]][a[1]] === board[c[0]][c[1]]
+      ) {
+        return board[a[0]][a[1]];
+      }
+    }
+    return null;
   }
 
   return (
@@ -26,6 +75,7 @@ function App() {
           <Player initialName="PLAYER 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>{winner} has won the game!</p>}
       </div>
       <Log turns={gameTurns} />
     </main>
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,12 +1,30 @@
-function Log({ turns }) {
+import PropTypes from "prop-types";
+
+function Log({ turns = [] }) {
   return (
-    <ol id="log">
-      {turns.map((turn) => (
-        <li key={`${turn.square.row}${turn.square.col}`}>
-          {turn.player}, selected {turn.square.row},{turn.square.col}
-        </li>
-      ))}
-    </ol>
+    <div id="log">
+      <h2>Game Log</h2>
+      <ul>
+        {turns.map((turn, index) => (
+          <li key={index}>
+            Player {turn.player} moved to ({turn.square.row}, {turn.square.col})
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
+
+Log.propTypes = {
+  turns: PropTypes.arrayOf(
+    PropTypes.shape({
+      square: PropTypes.shape({
+        row: PropTypes.number.isRequired,
+        col: PropTypes.number.isRequired,
+      }).isRequired,
+      player: PropTypes.string.isRequired,
+    })
+  ),
+};
+
 export default Log;
