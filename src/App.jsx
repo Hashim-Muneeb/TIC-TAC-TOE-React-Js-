a5060fe75f47531b57c89243d965fb10ef35412a
Hashim-Muneeb <hashimmuneeb75@gmail.com>
2024-08-07 12:17:25 +0500

    log screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Log from "./components/Log";
 import Player from "./components/PlayerInfo";
 import GameBoard from "./components/GameBoard";
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
   function handleSelectSquare(){
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
-
+    setGameTurns();
   }
 
 
